ab5a68e14723bc3ff994b695c5fefe603549cae6
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-02-15 00:04:24 +0530

    Update support channel and chat URLs

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lolspot")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/APNA_WORLD1")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
